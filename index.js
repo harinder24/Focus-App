fed6bf7be62ce8a30b00eef7642f6d3bf9db28a1
harinder24 <hss022002@gmail.com>
2023-02-03 18:46:25 -0800

    added movable marker
    marker moves as user moves

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 
-let latitude = 42;
-let longitude = -71;
-
+let latitude = 49.1369247;
+let longitude = -122.8341621;
+let currentLocation;
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
       console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-      let currentLocation = {lat:latitude,lng:longitude}
+      currentLocation = {lat:latitude,lng:longitude}
         let options = {
           zoom: 8,
           center: currentLocation
         }
         let map = new google.maps.Map(document.getElementById('gmp-map'),options)
         let marker = new google.maps.Marker({position: currentLocation, map: map});
-      
+        navigator.geolocation.watchPosition(function(position) {
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude;
+         
+          marker.setPosition({lat: latitude, lng: longitude});
+        
+          map.setCenter({lat: latitude, lng: longitude});
+        });
     });
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
+    
+  } 
 
 
