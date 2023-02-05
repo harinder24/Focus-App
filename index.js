1321eaa4ed4a88059c051f7cac39634d72771e93
harinder24 <hss022002@gmail.com>
2023-02-04 20:15:56 -0800

    reviews and wrrite review component working

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let locationData = [
       "https://i.insider.com/61b0fdd50ee53700184555cf?width=700",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1yf6H59J4yX8sdgkAmZCC_Tm7MTf01xoXPA&usqp=CAU",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.25743523738337, -123.1269753072887],
     idInfo:101
   },
@@ -22,6 +23,7 @@ let locationData = [
       "https://th.bing.com/th/id/R.ed6cc4eddec9ec1b4912237c93e4c865?rik=en5m3RdkWnh1aA&riu=http%3a%2f%2fwww.harvardmagazine.com%2fsites%2fdefault%2ffiles%2fimg%2farticle%2f0315%2fBodleian_1.jpg&ehk=3dIzHRkxlcCInQA67%2f%2foYm9diJHu7vmiQ%2bFWDVtRNkY%3d&risl=1&pid=ImgRaw&r=0",
       "https://th.bing.com/th/id/OIP.0v2zfmmaVAqUbkUmuWzlfQHaHa?pid=ImgDet&w=207&h=207&c=7&dpr=1.3",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.280049, -123.127559],
     idInfo:102
   },
@@ -34,6 +36,7 @@ let locationData = [
       "https://i.pinimg.com/originals/42/b6/f4/42b6f489e49f2569cc466ca871646a28.jpg",
       "https://gabreport.com/wp-content/uploads/2014/05/2-WBPL_Interior_1-550x439.jpg",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.251060, -123.062267],
     idInfo:103
   },
@@ -46,6 +49,7 @@ let locationData = [
       "https://upload.wikimedia.org/wikipedia/commons/a/a3/R%C3%B6e_g%C3%A5rd_caf%C3%A9_2.jpg",
       "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.blogto.com%2Feat_drink%2F2014%2F02%2Fthe_top_10_coffee_shops_that_serve_booze_in_toronto%2F&psig=AOvVaw3Jl9DNccXKCLBsBWZiUzNW&ust=1675570039207000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMCMmvr--vwCFQAAAAAdAAAAABAI",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.269631, -123.033597],
     idInfo:104
   },
@@ -58,6 +62,7 @@ let locationData = [
       "https://www.overseaseducationlane.com/university_galary_images/galary_image_4_1611311258.jpeg",
       "https://i.pinimg.com/736x/18/fa/1d/18fa1de78fec53c19e35ce2d19d40cab--toronto-branches.jpg",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.28159824977371, -123.11085230532682],
     idInfo:105
   },
@@ -69,6 +74,7 @@ let locationData = [
     img: [
       "https://parksinsandiego.com/wp-content/uploads/2019/04/cypress-canyon-park-uncovered-picnic-area-1024x768.jpg",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.358847481523966, -123.21076170726549],
     idInfo:106
   },
@@ -80,6 +86,7 @@ let locationData = [
     img: [
       "https://th.bing.com/th/id/OIP.YqitGwCgSCegQT8F4pOLTgHaCx?w=315&h=131&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     ],
+    review:[{name: "user1", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"},{name:"user2",content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"}],
     location: [49.277522282368565, -123.15346887212631],
     idInfo:107
   },
@@ -190,6 +197,9 @@ function initMap() {
       let imgStarString = ""
       for(let j = 0; j < star; j++){
         imgStarString += '<img src = "https://blake5kh.files.wordpress.com/2012/09/star.jpg" width="10px">'
+      }
+      for(let j = star; j < 5; j++){
+        imgStarString += '<img src = "https://www.freeiconspng.com/uploads/gold-star-empty-image-icon-23.png" width="10px">'
       }
       let content = `<div class="buttonClick"><img src = ${arrayOfObj[i].img[0]} width="100px"> <h4>${arrayOfObj[i].name}</h4></div><div>Ratings: ${imgStarString}</div><div id="${arrayOfObj[i].idInfo}" onclick = "idk(this)" ><button>Book!</button></div>`;
      
--- location.js
+++ location.js
@@ -8,7 +8,7 @@ for (let i = 0; i < allLocationInfo.length; i++) {
     }
     
 }
-
+let truth = false;
 let slideshowContainer = document.createElement("div");
 slideshowContainer.classList.add("slideshow-container");
 
@@ -26,7 +26,8 @@ for (let i = 0; i < locationInfo.img.length; i++) {
 
   let image = document.createElement("img");
   image.src = locationInfo.img[i];
-  image.style.width = "100%";
+  
+  // image.style.width = "100%";
   slide.appendChild(image);
 
   mySlides.push(slide);
@@ -79,5 +80,116 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
+document.getElementById('lname').innerHTML = `<b><i>${locationInfo.name}</i></b>`
 showSlides()
+
+function loadReview() {
+  let numOfRating = 0;
+  let star = 0;
+  for(let j =0; j < locationInfo.rating.length; j++){
+    numOfRating += locationInfo.rating[j]
+  }
+  numOfRating = numOfRating/locationInfo.rating.length;
+  while(numOfRating != 0){
+    if(numOfRating - 1 > 0){
+      numOfRating--;
+      star++;
+    }else{
+      if(numOfRating >= 0.5){
+        numOfRating--;
+        star++;
+      }else{
+        numOfRating = 0;
+      }
+    }
+  }
+  let imgStarString = ""
+  for(let j = 0; j < star; j++){
+    imgStarString += '<img src = "https://blake5kh.files.wordpress.com/2012/09/star.jpg" width="40px">'
+  }
+  for(let j = star; j < 5; j++){
+    imgStarString += '<img src = "https://www.freeiconspng.com/uploads/gold-star-empty-image-icon-23.png" width="40px">'
+  }
+  let rev = document.getElementById("rev")
+  imgStarString = `<div><b>Rating</b> ${imgStarString}</div>`
+  let reviewSection = ""
+  for(let i = locationInfo.review.length-1; i >= 0 ; i--){
+    reviewSection += `<div class="message"><i>${locationInfo.review[i].name}~</i><br><p>${locationInfo.review[i].content}</p></div>`
+  }
+  rev.innerHTML = imgStarString + reviewSection
+}
+let numRating = 5;
+function writeReview(){
+  // const ratingContainer = document.createElement("div");
+  // ratingContainer.classList.add("rating");
+  let starsString = "<div class = 'rating'><h2>How was your experience</h2>";
+  for (let i = 0; i < 5; i++) {
+    starsString += "<span class = 'star'>&#9733;</span>"
+  }
+  starsString += `</div><div class="review-box"><textarea id="userInput" placeholder="Write your review here"></textarea><div id='bravo'><br></div><button id="btn1">Submit</button></div>`
+  
+  document.getElementById('wrev').innerHTML = starsString;
+  const rating = document.querySelector('.rating');
+  const stars = Array.from(rating.querySelectorAll('.star'));
+  
+  rating.addEventListener('click', (e) => {
+    
+  const index = stars.indexOf(e.target);
+  stars.forEach((star, i) => {
+    if (i <= index) {
+      star.style.color = '#f2b01e';
+    } else {
+      star.style.color = '#bbb';
+    }
+  });
+  numRating = index+1;
+  truth = true
+});
+
+
+btn1.addEventListener("click",function () {
+  let input = document.getElementById('userInput').value;
+  if(truth === true){
+    if(input == ""){
+      for (let i = 0; i < allLocationInfo.length; i++) {
+        if(allLocationInfo[i].idInfo == target){
+            allLocationInfo[i].rating.push(numRating)
+            locationInfo = allLocationInfo[i]
+            break;
+        }
+    }
+    }else{
+      for (let i = 0; i < allLocationInfo.length; i++) {
+        if(allLocationInfo[i].idInfo == target){
+            allLocationInfo[i].rating.push(numRating)
+            allLocationInfo[i].review.push({name:"tester",content:input})
+            locationInfo = allLocationInfo[i]
+            break;
+        }
+    }}
+    input = ""
+    numRating = 5;
+    let bravo = document.getElementById('bravo')
+    bravo.innerHTML = "Bravo! Thanks for review"
+    bravo.style.color = "green"
+    truth = false;
+  }else{
+    let bravo = document.getElementById('bravo')
+    bravo.innerHTML = "Choose a rating!"
+    bravo.style.color = "red"
+  }
+  
+})
+
+}
+
+document.getElementById('review').addEventListener('click',function(){
+  document.getElementById('booking').innerHTML = null;
+  document.getElementById('wrev').innerHTML = null;
+  loadReview();
+})
+document.getElementById('add').addEventListener('click',function(){
+  document.getElementById('booking').innerHTML = null;
+  document.getElementById('rev').innerHTML = null;
+  writeReview();
+})
